1021c93e7ba63644c551de8bd62f4660fe725a1e
Rawaha-mr11 <rawahamr11@gmail.com>
2025-12-14 17:43:11 +0500

    Contact.jsx
    Updated on 12 Dec, 2025

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -31,112 +31,106 @@ const Contact = () => {
   };
 
   return (
-    <>
-      <section id="Contact" className="w-full p-2 sm:p-4 mb-40">
-        <div className="w-full p-2 mt-16 sm:p-4 flex flex-col justify-center items-center">
-          <p className="text-text-secondary uppercase text-sm sm:text-base">
-            Contact
-          </p>
-          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl leading-tight text-center mt-2 sm:mt-4">
-            Ready to Start Your Project?
-          </h1>
+    <section id="Contact" className="w-full p-4 sm:p-6 lg:p-10 mb-32">
+      <div className="w-full mt-16 flex flex-col justify-center items-center text-center">
+        <p className="text-text-secondary uppercase text-sm sm:text-base">
+          Contact
+        </p>
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl mt-4">
+          Ready to Start Your Project?
+        </h1>
+      </div>
+
+      <div className="w-full mt-16 flex flex-wrap justify-center gap-8">
+        
+        <div className="w-full lg:w-1/3">
+          <div className="bg-card-bg p-6 sm:p-10 lg:p-16 flex flex-col gap-12">
+
+            <div className="text-text-secondary hover:text-text-primary border-b-[1.5px] border-card pb-6">
+              <CallIcon fontSize="large" />
+              <h2 className="text-xl text-text-primary font-bold mt-5">
+                Contact Number :
+              </h2>
+              <a href="tel:+923028053159" className="block mt-2">
+                +92 302 80 53159
+              </a>
+            </div>
+
+            <div className="text-text-secondary hover:text-text-primary border-b-[1.5px] border-card pb-6">
+              <MailOutlineIcon fontSize="large" />
+              <h2 className="text-xl text-text-primary font-bold mt-5">
+                Email Us :
+              </h2>
+              <a
+                href="mailto:rawahamr11@gmail.com"
+                className="block mt-2"
+              >
+                rawahamr11@gmail.com
+              </a>
+            </div>
+
+          </div>
         </div>
 
-        <div className="w-full p-4 flex justify-center gap-8 ">
-          <div className="w-1/3 flex flex-col flex-wrap">
-            <div className="bg-card-bg p-16">
-              <div className="p-4 text-text-secondary hover:text-text-primary border-b-[1.5px] border-card">
-                <div>
-                  <CallIcon fontSize="large" />
-                </div>
-                <h1 className="text-xl text-text-primary font-bold mt-5">
-                  Contact Number :
-                </h1>
-                <a href="tel: +92 302 80 53159" className="text-text-secondary">
-                  +92 302 80 53159
-                </a>
-              </div>
+        <div className="w-full lg:w-1/2 border-[1.5px] border-card bg-neo p-6 sm:p-8 lg:p-10">
+          <form className="flex flex-col gap-8 font-syne">
 
-              <div className="w-4/7 p-4 mt-16 text-text-secondary hover:text-text-primary border-b-[1.5px] border-card">
-                <div>
-                  <MailOutlineIcon fontSize="large" />
-                </div>
-                <h1 className="text-xl text-text-primary font-bold mt-5">
-                  Email Us :
-                </h1>
-                <a
-                  href="mailto:rawahamr11@gmail.com"
-                  className="text-text-secondary"
-                >
-                  rawahamr11@gmail.com
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div className="w-1/2 border-[1.5px] border-card bg-neo p-8">
-            <form action="" className="flex flex-col gap-8 font-syne">
-              <div className="w-full flex gap-4">
-                <TextField
-                  label="Your Name"
-                  variant="outlined"
-                  required
-                  fullWidth
-                  margin="normal"
-                  sx={inputStyle}
-                />
-                <TextField
-                  label="Your Email"
-                  type="email"
-                  required
-                  variant="outlined"
-                  fullWidth
-                  margin="normal"
-                  sx={inputStyle}
-                />
-              </div>
-
+            <div className="w-full flex flex-col sm:flex-row gap-4">
               <TextField
-                label="Your Subject"
-                type="text"
+                label="Your Name"
                 required
-                variant="outlined"
                 fullWidth
-                margin="normal"
                 sx={inputStyle}
               />
+              <TextField
+                label="Your Email"
+                type="email"
+                required
+                fullWidth
+                sx={inputStyle}
+              />
+            </div>
 
-              <div className="w-full">
-                <div className="relative w-full">
-                  <textarea
-                    id="your-Message"
-                    name="your-Message"
-                    required
-                    placeholder=" "
-                    rows={5}
-                    className="w-full peer flex rounded-md border border-text-secondary bg-transparent px-3 pt-5 pb-2 text-sm text-text-primary
-                 focus:border-text-primary transition-all duration-200 font-syne"
-                  ></textarea>
+            <TextField
+              label="Your Subject"
+              required
+              fullWidth
+              sx={inputStyle}
+            />
 
-                  <label
-                    htmlFor="your-Message"
-                    className="absolute left-3 top-0 px-1 text-text-secondary text-sm bg-neo transition-all duration-200
-                 peer-placeholder-shown:top-5 peer-placeholder-shown:text-text-secondary peer-placeholder-shown:text-base
-                 peer-focus:-top-3 peer-focus:text-text-secondary peer-focus:text-[0.8rem]"
-                  >
-                    Describe Your Project
-                  </label>
-                </div>
-              </div>
+            <div className="relative w-full">
+              <textarea
+                id="your-message"
+                required
+                rows={5}
+                placeholder=" "
+                className="w-full peer rounded-md border border-text-secondary bg-transparent px-3 pt-5 pb-2 text-sm text-text-primary
+                focus:border-text-primary transition-all duration-200 font-syne"
+              ></textarea>
 
-              <button className="w-fit mt-6 sm:mt-8 lg:mt-10 p-4 sm:p-5 text-sm font-semibold text-text-primary border-[1px] border-[var(--accent-primary)] bg-[var(--accent-primary)] transition-all duration-[400ms] hover:text-text-primary hover:border-[var(--accent-hover)] hover:bg-transparent flex items-center justify-center gap-2">
-                Message <MailOutlineIcon fontSize="small" />
-              </button>
-            </form>
-          </div>
+              <label
+                htmlFor="your-message"
+                className="absolute left-3 top-0 px-1 text-text-secondary text-sm bg-neo transition-all duration-200
+                peer-placeholder-shown:top-5 peer-placeholder-shown:text-base
+                peer-focus:-top-3 peer-focus:text-[0.8rem]"
+              >
+                Describe Your Project *
+              </label>
+            </div>
+
+            <button className="w-fit mt-6 p-4 sm:p-5 text-sm font-semibold
+              text-text-primary border border-[var(--accent-primary)]
+              bg-[var(--accent-primary)]
+              transition-all duration-300
+              hover:bg-transparent hover:border-[var(--accent-hover)]
+              flex items-center gap-2">
+              Message <MailOutlineIcon fontSize="small" />
+            </button>
+
+          </form>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
